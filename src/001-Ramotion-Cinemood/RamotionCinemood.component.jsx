27ffcc9899465085eb80cb212b378509c80557dd
Steve Tyree <hello@stevetyree.com>
2017-08-26 14:53:28 -0700

    Left hand controls

--- src/001-Ramotion-Cinemood/RamotionCinemood.component.jsx
+++ src/001-Ramotion-Cinemood/RamotionCinemood.component.jsx
@@ -8,11 +8,33 @@ export default class RamotionCinemood extends React.Component {
 		return (
 			<div className="cinemood">
 				<div className="cinemood__inner-left">
-					<div className="tempImage"></div>
-					<div className="tempImage tempImage--main">
-						<img src="https://cinemood.com/wp-content/themes/focux-child/assets/cwe-278/build/img/pictures/bottom-cinemood.png" height="100%" width="100%" alt=""/>
+					<div className="tempImage">
+						<img
+							src="https://cinemood.com/wp-content/themes/focux-child/assets/cwe-278/build/img/pictures/cinemood-pack1.png"
+							height="100%" width="100%" alt=""/>
 					</div>
-					<div className="tempImage"></div>
+					<div className="cinemood__image-path"></div>
+					<div className="tempImage tempImage--main">
+						<img
+							src="https://cinemood.com/wp-content/themes/focux-child/assets/cwe-278/build/img/pictures/bottom-cinemood.png"
+							height="100%" width="100%" alt=""/>
+					</div>
+					<div className="cinemood__slide-control">
+						<div className="cinemood__slide-control__item">
+							<span></span>
+						</div>
+						<div className="cinemood__slide-control__item cinemood__slide-control__item--active">
+							<span></span>
+						</div>
+						<div className="cinemood__slide-control__item">
+							<span></span>
+						</div>
+					</div>
+					<div className="tempImage">
+						<img
+							src="https://cinemood.com/wp-content/themes/focux-child/assets/cwe-278/build/img/pictures/cinemood-pack1-hover.png"
+							height="100%" width="100%" alt=""/>
+					</div>
 				</div>
 				<div className="cinemood__inner-right">
 					<div style={{ height: '100%', width: 150 }}>
@@ -46,20 +68,29 @@ export default class RamotionCinemood extends React.Component {
 						<div className="cinemood__blurbs">
 							<div className="cinemood__blurbs__item">
 								<EyeIcon/>
-								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusanti doloremque.</p>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptat accusanti
+									doloremque.</p>
 							</div>
 							<div className="cinemood__blurbs__item">
 								<BatteryIcon/>
-								<p>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.</p>
+								<p>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+									beatae.</p>
 							</div>
 							<div className="cinemood__blurbs__item">
 								<WifiIcon/>
-								<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+								<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed
+									quia.</p>
 							</div>
 							<div className="cinemood__blurbs__item">
 								<AlarmIcon/>
-								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+									laboriosam.</p>
 							</div>
+						</div>
+						<div className="cinemood__blurbs__see-more">
+							<hr/>
+							<span>See more</span>
+							<hr/>
 						</div>
 						<div className="cinemood__content-actions">
 							<button className="primary">Add to cart</button>
